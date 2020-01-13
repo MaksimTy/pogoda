fb92094be8c6fc2a019d1d9ac8e46499ad4a2370
Maksim Tymkovskiy <maksimvt@gmail.com>
2020-01-14 00:30:13 +0300

    scheduledExecutorService

--- src/main/java/gui/GUIWindow.java
+++ src/main/java/gui/GUIWindow.java
@@ -5,9 +5,7 @@ import javafx.geometry.Insets;
 import javafx.scene.layout.HBox;
 import parser.Parser;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 
 public class GUIWindow extends HBox {
@@ -16,7 +14,7 @@ public class GUIWindow extends HBox {
     private GUIForecast guiForecast;
     private Parser parser;
 
-    public GUIWindow() {
+    public GUIWindow() throws ExecutionException, InterruptedException {
         this.parser = new Parser();
         this.guiButtons = new GUIButtons(this.parser);
         try {
@@ -26,14 +24,30 @@ public class GUIWindow extends HBox {
         }
         this.guiButtons.setGuiForecast(this.guiForecast);
 
-        this.getChildren().add(0,this.guiButtons);
-        this.getChildren().add(1,this.guiForecast);
+        this.getChildren().add(0, this.guiButtons);
+        this.getChildren().add(1, this.guiForecast);
 
         //выравнивает блок кнопок по уровню с блоком таблицы.
-        HBox.setMargin(this.guiButtons, new Insets(this.guiForecast.getTopIndent(), 0,0,0));
+        HBox.setMargin(this.guiButtons, new Insets(this.guiForecast.getTopIndent(), 0, 0, 0));
+        // обновлять Parser через каждые 30 мин.
+        this.refreshParser();
+    }
 
-        Executor executor = Executors.newSingleThreadExecutor();
-
-
+    /**
+     * Метод обновляет Parser каждые 30 минут.
+     */
+    private void refreshParser() {
+        System.out.println("getParser");
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                parser = new Parser();
+                System.out.println(parser.getFirstDate());
+                System.out.println("delay");
+            }
+        };
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        scheduledExecutorService.scheduleWithFixedDelay(task, 30, 30, TimeUnit.MINUTES);
+        System.out.println("getParser");
     }
 }
